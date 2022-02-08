2740f43f014692cf73a3d7884f9e076facf0605c
brhliluk <brhliluk@fit.cvut.cz>
2022-02-08 09:15:08 +0100

    Cleanup

--- app/src/main/java/cz/brhliluk/android/praguewaste/ui/view/GoogleMapsView.kt
+++ app/src/main/java/cz/brhliluk/android/praguewaste/ui/view/GoogleMapsView.kt
@@ -1,88 +1,13 @@
 package cz.brhliluk.android.praguewaste.ui.view
 
-import android.os.Bundle
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.*
-import cz.brhliluk.android.praguewaste.R
-import cz.brhliluk.android.praguewaste.model.Bin
 import cz.brhliluk.android.praguewaste.utils.location
 import cz.brhliluk.android.praguewaste.viewmodel.MainViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
-
-@Composable
-fun GoogleMaps() {
-    val mapView = rememberMapViewWithLifeCycle()
-    Box {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.White)
-        ) {
-            AndroidView({ mapView }) { mapView ->
-                CoroutineScope(Dispatchers.Main).launch {
-                    //noinspection MissingPermission
-                    mapView.getMapAsync {
-                        it.mapType = 1
-                        it.uiSettings.isZoomControlsEnabled = true
-                        it.uiSettings.isMyLocationButtonEnabled = true
-                        it.isMyLocationEnabled = true
-                    }
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun rememberMapViewWithLifeCycle(): MapView {
-    val context = LocalContext.current
-    val mapView = remember {
-        MapView(context).apply {
-            id = R.id.map_frame
-        }
-    }
-    val lifeCycleObserver = rememberMapLifecycleObserver(mapView)
-    val lifeCycle = LocalLifecycleOwner.current.lifecycle
-    DisposableEffect(lifeCycle) {
-        lifeCycle.addObserver(lifeCycleObserver)
-        onDispose { lifeCycle.removeObserver(lifeCycleObserver) }
-    }
-
-    return mapView
-}
-
-@Composable
-fun rememberMapLifecycleObserver(mapView: MapView): LifecycleEventObserver =
-    remember(mapView) {
-        LifecycleEventObserver { _, event ->
-            when (event) {
-                Lifecycle.Event.ON_CREATE -> mapView.onCreate(Bundle())
-                Lifecycle.Event.ON_START -> mapView.onStart()
-                Lifecycle.Event.ON_RESUME -> mapView.onResume()
-                Lifecycle.Event.ON_PAUSE -> mapView.onPause()
-                Lifecycle.Event.ON_STOP -> mapView.onStop()
-                Lifecycle.Event.ON_DESTROY -> mapView.onDestroy()
-                else -> throw IllegalStateException()
-            }
-        }
-    }
 
 @Composable
 fun WasteGoogleMap(vm: MainViewModel) {
@@ -100,26 +25,6 @@ fun WasteGoogleMap(vm: MainViewModel) {
         )
     }
 
-    // Allows camera to follow user
-//    LaunchedEffect(vm) {
-//        snapshotFlow { currentLocation }
-//            .collect {
-//                cameraPositionState.position = CameraPosition.fromLatLngZoom(
-//                    LatLng(
-//                        currentLocation.latitude,
-//                        currentLocation.longitude
-//                    ), 15f
-//                )
-//            }
-//    }
-//
-//    LaunchedEffect(vm) {
-//        snapshotFlow { cameraPositionState.position }
-//            .collect {
-//                vm.location.value = LatLng(it.target.latitude, it.target.longitude)
-//            }
-//    }
-
     val currentBins by vm.currentBins.collectAsState()
 
     GoogleMap(
@@ -130,7 +35,7 @@ fun WasteGoogleMap(vm: MainViewModel) {
     ) {
         currentBins.forEach { bin ->
             // TODO icon, onclick
-            Marker(position = bin.location, title = bin.address, )
+            Marker(position = bin.location, title = bin.address)
         }
     }
 }
--- app/src/main/java/cz/brhliluk/android/praguewaste/utils/Extensions.kt
+++ app/src/main/java/cz/brhliluk/android/praguewaste/utils/Extensions.kt
@@ -3,4 +3,5 @@ package cz.brhliluk.android.praguewaste.utils
 import com.google.android.gms.maps.model.LatLng
 import cz.brhliluk.android.praguewaste.model.Bin
 
+//TODO fix api
 val Bin.location: LatLng get() = LatLng(longitude, latitude)
