6f6d62e9b8a3b13c55b7f796f4cbb2a3ec50f830
brhliluk <brhliluk@fit.cvut.cz>
2022-03-02 12:02:45 +0100

    Refactor and style Bottom nav bar

--- app/src/main/java/cz/brhliluk/android/praguewaste/ui/view/BottomNavigation.kt
+++ app/src/main/java/cz/brhliluk/android/praguewaste/ui/view/BottomNavigation.kt
@@ -1,18 +1,26 @@
 package cz.brhliluk.android.praguewaste.ui.view
 
 import android.content.Intent
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.ModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import cz.brhliluk.android.praguewaste.R
 import cz.brhliluk.android.praguewaste.ui.activity.SettingsActivity
+import cz.brhliluk.android.praguewaste.ui.theme.VeryLightGray
 import cz.brhliluk.android.praguewaste.utils.onClick
 import cz.brhliluk.android.praguewaste.viewmodel.BottomSheet
 import cz.brhliluk.android.praguewaste.viewmodel.MainViewModel
@@ -23,40 +31,35 @@ fun BottomNavBar(vm: MainViewModel, sheetState: ModalBottomSheetState) {
     val context = LocalContext.current
     val coroutineScope = rememberCoroutineScope()
 
-    Row (Modifier
-        .fillMaxWidth()
-        .background(color = Color.LightGray)
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .background(color = VeryLightGray)
+            .height(60.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        IconButton(onClick = {
+        BottomNavIcon(R.drawable.ic_search, Modifier.weight(1f)) {
             vm.activeBottomSheet = vm.activeBottomSheet.onClick(BottomSheet.SEARCH, coroutineScope, sheetState)
-        }, Modifier.weight(1f)) {
-            Icon(
-                painter = painterResource(R.drawable.ic_search),
-                contentDescription = null
-            )
         }
-
-        IconButton(onClick = {
+        BottomNavIcon(R.drawable.ic_nearby, Modifier.weight(1f)) {
             vm.activeBottomSheet = vm.activeBottomSheet.onClick(BottomSheet.NEAR, coroutineScope, sheetState)
-        }, Modifier.weight(1f)) {
-            Icon(
-                painter = painterResource(R.drawable.ic_nearby),
-                contentDescription = null
-            )
         }
+        BottomNavIcon(R.drawable.ic_settings, Modifier.weight(1f)) {
+            context.startActivity(Intent(context, SettingsActivity::class.java))
+        }
+    }
+}
 
-        IconButton(onClick = {
-            context.startActivity(
-                Intent(
-                    context,
-                    SettingsActivity::class.java
-                )
-            )
-        }, Modifier.weight(1f)) {
-            Icon(
-                painter = painterResource(R.drawable.ic_settings),
-                contentDescription = null
-            )
-        }
+@Composable
+fun BottomNavIcon(@DrawableRes icon: Int, modifier: Modifier, onIconClick: () -> Unit) {
+    IconButton(
+        onClick = onIconClick,
+        modifier = modifier,
+    ) {
+        Icon(
+            painter = painterResource(icon),
+            contentDescription = null,
+            tint = Color.Black
+        )
     }
 }
