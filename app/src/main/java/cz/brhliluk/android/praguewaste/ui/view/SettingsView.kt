eb949d3251d36c68c19e57c2803a9a19b093e9c3
brhliluk <brhliluk@fit.cvut.cz>
2022-03-10 17:19:49 +0100

    Add app bar to settings view

--- app/src/main/java/cz/brhliluk/android/praguewaste/ui/view/SettingsView.kt
+++ app/src/main/java/cz/brhliluk/android/praguewaste/ui/view/SettingsView.kt
@@ -1,12 +1,14 @@
 package cz.brhliluk.android.praguewaste.ui.view
 
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Switch
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -21,14 +23,29 @@ import cz.brhliluk.android.praguewaste.viewmodel.SettingsViewModel
 fun SettingsView(vm: SettingsViewModel) {
 
     val locationEnabled = vm.isLocationEnabledAsFlow().collectAsState(initial = true)
+    val dispatcher = LocalOnBackPressedDispatcherOwner.current!!.onBackPressedDispatcher
 
-    Scaffold {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(stringResource(R.string.settings)) }, navigationIcon = {
+                IconButton(onClick = { dispatcher.onBackPressed() }) {
+                    Icon(Icons.Filled.ArrowBack, "backIcon")
+                }
+            })
+        }
+    ) {
         Column(
             Modifier
                 .fillMaxSize()
-                .padding(12.dp)) {
-            Text(stringResource(R.string.app_name), fontSize = 25.sp, fontWeight = FontWeight.Bold, modifier = Modifier.padding(bottom = 10.dp))
-            Row (verticalAlignment = Alignment.CenterVertically) {
+                .padding(12.dp)
+        ) {
+            Text(
+                stringResource(R.string.app_name),
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 10.dp)
+            )
+            Row(verticalAlignment = Alignment.CenterVertically) {
                 Text(text = stringResource(R.string.use_location))
                 Spacer(modifier = Modifier.weight(1f))
                 Switch(checked = locationEnabled.value, onCheckedChange = { vm.setLocationEnabled(it) })
