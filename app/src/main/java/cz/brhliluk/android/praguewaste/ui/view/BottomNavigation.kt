b63f461b61a87f6bbe663617e3e17c3ed773df51
brhliluk <brhliluk@fit.cvut.cz>
2022-03-10 21:16:14 +0100

    Nicer bottom navbar

--- app/src/main/java/cz/brhliluk/android/praguewaste/ui/view/BottomNavigation.kt
+++ app/src/main/java/cz/brhliluk/android/praguewaste/ui/view/BottomNavigation.kt
@@ -1,21 +1,30 @@
 package cz.brhliluk.android.praguewaste.ui.view
 
 import android.content.Intent
-import androidx.annotation.DrawableRes
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.BottomSheetScaffoldState
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.LocationOn
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import cz.brhliluk.android.praguewaste.R
 import cz.brhliluk.android.praguewaste.ui.activity.SettingsActivity
 import cz.brhliluk.android.praguewaste.ui.theme.VeryLightGray
 import cz.brhliluk.android.praguewaste.utils.onClick
@@ -24,17 +33,31 @@ import cz.brhliluk.android.praguewaste.viewmodel.MainViewModel
 
 
 @Composable
-fun BottomNavIcon(@DrawableRes icon: Int, modifier: Modifier, onIconClick: () -> Unit) {
-    IconButton(
-        onClick = onIconClick,
-        modifier = modifier,
+fun BottomNavIcon(icon: ImageVector, selectedIcon: ImageVector, modifier: Modifier, selected: Boolean, onIconClick: () -> Unit) {
+    Box(
+        modifier,
+        contentAlignment = Alignment.Center
     ) {
-        Icon(
-            painter = painterResource(icon),
-            contentDescription = null,
-            tint = Color.Black
-        )
+        AnimatedVisibility(selected) {
+            Box(
+                modifier = Modifier
+                    .width(icon.viewportWidth.dp + 40.dp)
+                    .height(icon.viewportHeight.dp + 20.dp)
+                    .clip(CircleShape)
+                    .background(Color.LightGray)
+            )
+        }
+        IconButton(
+            onClick = onIconClick
+        ) {
+            Icon(
+                imageVector = if (selected) selectedIcon else icon,
+                contentDescription = null,
+                tint = Color.Black,
+            )
+        }
     }
+
 }
 
 @ExperimentalMaterialApi
@@ -50,13 +73,13 @@ fun BottomNavBar(vm: MainViewModel, sheetScaffoldState: BottomSheetScaffoldState
             .height(60.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        BottomNavIcon(R.drawable.ic_search, Modifier.weight(1f)) {
+        BottomNavIcon(Icons.Outlined.Search, Icons.Filled.Search, Modifier.weight(1f), vm.activeBottomSheet == BottomSheet.SEARCH) {
             vm.activeBottomSheet = vm.activeBottomSheet.onClick(BottomSheet.SEARCH, coroutineScope, sheetScaffoldState)
         }
-        BottomNavIcon(R.drawable.ic_nearby, Modifier.weight(1f)) {
+        BottomNavIcon(Icons.Outlined.LocationOn, Icons.Filled.LocationOn, Modifier.weight(1f), vm.activeBottomSheet == BottomSheet.NEAR) {
             vm.activeBottomSheet = vm.activeBottomSheet.onClick(BottomSheet.NEAR, coroutineScope, sheetScaffoldState)
         }
-        BottomNavIcon(R.drawable.ic_settings, Modifier.weight(1f)) {
+        BottomNavIcon(Icons.Outlined.Settings, Icons.Filled.Settings, Modifier.weight(1f), false) {
             context.startActivity(Intent(context, SettingsActivity::class.java))
         }
     }
