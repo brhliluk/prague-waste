38de47753987c2d636a66bd55e39e74d9566eedf
brhliluk <brhliluk@fit.cvut.cz>
2022-03-21 14:18:58 +0100

    Display trash types rather than distance

--- watch/src/main/java/cz/brhliluk/android/watch/ui/view/BinItemView.kt
+++ watch/src/main/java/cz/brhliluk/android/watch/ui/view/BinItemView.kt
@@ -1,11 +1,14 @@
 package cz.brhliluk.android.watch.ui.view
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.Chip
@@ -26,18 +29,11 @@ fun BinItemView(userLocation: LatLng, bin: Bin, onClick: (Bin) -> Unit) {
         onClick = { TODO() },
         label = { Text(bin.address, overflow = TextOverflow.Ellipsis, maxLines = 1) },
         secondaryLabel = {
-            Text(text = "+${String.format("%.1f", (bin.distanceFrom(userLocation) / 1000))}km")
-//            LazyRow(Modifier.padding(bottom = 10.dp, top = 5.dp)) {
-//                items(items = bin.namedTrashTypes, itemContent = { trashType ->
-//                    Box(
-//                        modifier = Modifier
-//                            .padding(5.dp)
-//                            .size(10.dp)
-//                            .clip(CircleShape)
-//                            .background(trashType?.color ?: Color.Transparent)
-//                    )
-//                })
-//            }
+            LazyRow(Modifier.padding(bottom = 10.dp, top = 5.dp)) {
+                items(items = bin.namedTrashTypes, itemContent = { trashType ->
+                    Text("●", overflow = TextOverflow.Visible, color = trashType?.color ?: Color.Transparent)
+                })
+            }
         }
     )
 }
---
+++ watch/src/main/java/cz/brhliluk/android/watch/ui/view/BinView.kt
@@ -0,0 +1,9 @@
+package cz.brhliluk.android.watch.ui.view
+
+import androidx.compose.runtime.Composable
+import cz.brhliluk.android.praguewaste.common.model.Bin
+
+@Composable
+fun BinView(bin: Bin) {
+
+}
