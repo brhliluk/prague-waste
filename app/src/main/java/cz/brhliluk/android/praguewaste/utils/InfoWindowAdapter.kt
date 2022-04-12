76a1ea07e2bfc15cef6825e743779344e781e9c6
brhliluk <brhliluk@fit.cvut.cz>
2022-04-12 17:11:01 +0200

    Add navigation button

--- app/src/main/java/cz/brhliluk/android/praguewaste/utils/InfoWindowAdapter.kt
+++ app/src/main/java/cz/brhliluk/android/praguewaste/utils/InfoWindowAdapter.kt
@@ -1,8 +1,11 @@
 package cz.brhliluk.android.praguewaste.utils
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.IdRes
@@ -15,13 +18,21 @@ import kotlinx.serialization.json.Json
 import org.koin.core.component.KoinComponent
 
 // Couldn't find a way to use compose here
-class InfoWindowAdapter(context: Context) : GoogleMap.InfoWindowAdapter, KoinComponent {
+class InfoWindowAdapter(private val context: Context) : GoogleMap.InfoWindowAdapter, KoinComponent {
     private val layoutInflater = LayoutInflater.from(context)
     private val view: View = layoutInflater.inflate(R.layout.marker_info_window, null)
 
     private fun setInfoWindowText(marker: Marker) {
         val trashTypesList = Json.decodeFromString<List<Int>>(marker.snippet!!).map { Bin.TrashType.byId(it) }
         view.findViewById<TextView>(R.id.markerInfoTitle).text = marker.title
+        view.findViewById<Button>(R.id.navButton).setOnClickListener {
+            val mapIntent = Intent(Intent.ACTION_VIEW, Uri.parse("google.navigation:q=${marker.position.latitude},${marker.position.longitude}"))
+            mapIntent.setPackage("com.google.android.apps.maps")
+            mapIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            if (mapIntent.resolveActivity(context.packageManager) != null) {
+                context.startActivity(mapIntent)
+            }
+        }
         // This is stupid but databinding didn't work for me here
         setViewVisible(R.id.paper, Bin.TrashType.PAPER, trashTypesList)
         setViewVisible(R.id.plastic, Bin.TrashType.PLASTIC, trashTypesList)
